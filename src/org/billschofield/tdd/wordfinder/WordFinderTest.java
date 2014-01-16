02b6d8f3cb5d41b628da14580522f08297b036fc
Bill Schofield <bill@billschofield.net>
2014-01-16 15:13:14 -0600

    Added temporary comments to label parts of the test.

--- src/org/billschofield/tdd/wordfinder/WordFinderTest.java
+++ src/org/billschofield/tdd/wordfinder/WordFinderTest.java
@@ -11,13 +11,30 @@ import static org.junit.Assert.assertThat;
 public class WordFinderTest {
 
     @Test
-    public void shouldFindWordWhenTheEntireStringIsADictionaryWord() {
+    public void shouldFindWordWhenTheEntireStringIsADictionaryWordGWT() {
+        // Given
         String theWord = "paisley";
         List<String> dictionary = new ArrayList<String>();
         WordFinder wordFinder = new WordFinder(dictionary);
 
+        // When
         String wordsInDictionary = wordFinder.findWordsIn(theWord);
 
+        //Then
+        assertThat(wordsInDictionary, is(theWord));
+    }
+
+    @Test
+    public void shouldFindWordWhenTheEntireStringIsADictionaryWordAAA() {
+        // Arrange objects
+        String theWord = "paisley";
+        List<String> dictionary = new ArrayList<String>();
+        WordFinder wordFinder = new WordFinder(dictionary);
+
+        // perform Action
+        String wordsInDictionary = wordFinder.findWordsIn(theWord);
+
+        // Assert correct result
         assertThat(wordsInDictionary, is(theWord));
     }
 }
